7fe8fa859495406cb0b22767c7d0db04a58d34a1
Rollet Fabien <fabrol47@gmail.com>
2024-06-03 23:28:55 +0200

    updated memento css

--- src/components/Mémento.jsx
+++ src/components/Mémento.jsx
@@ -2,6 +2,7 @@ export default function Mémento() {
   return (
     <section className="bg-secondary-color p-8 memento">
       <h2>Mémento</h2>
+      <img src="../profil.png" alt="" />
       <p>
         Suite à une licence en science de la vie et quelques expériences
         professionnelles, j’ai décidé de me reconvertir dans le numérique pour
@@ -10,7 +11,6 @@ export default function Mémento() {
         Je suis à la recherche d’une équipe au sein de laquelle je pourrai
         m’investir et qui me permettra de progresser en tant que développeur.
       </p>
-      <img src="../profil.jpg" alt="" />
     </section>
   );
 }

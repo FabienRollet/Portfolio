87910a675b8bcc0a976c3505f76e23b71163ac73
Fabien Rollet <156512541+FabienRollet@users.noreply.github.com>
2024-09-02 15:43:48 +0200

    Update title.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <header className="bg-primary-color font-bold p-8 ">
-      <h1 className="font-font-header text-6xl pb-6 max-w-2">Fabien Rollet</h1>
+      <h1 className="font-font-header text-6xl pb-6 max-w-2">Fabien Rollet | Développeur Web Full Stack</h1>
       <p className="text-xl">
         Bonjour et bienvenue ! Prenez le temps de découvrir mon travail, et
         sentez-vous libre de me contacter :)

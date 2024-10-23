1500b67ee011218c3a9fee0e873a8ba4802d173f
Fabien Rollet <156512541+FabienRollet@users.noreply.github.com>
2024-10-23 13:50:20 +0200

    fix css

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -52,13 +52,13 @@ export default function Projets() {
         </article>
       </a>
       <a href="https://les-devs-juniors.vercel.app/" target="_blank">
-        <article className="projet lock">
+        <article className="projet">
           <h3>Les devs juniors</h3>
           <p>
            Collectif de jeune développeurs qui propose la création de site
            vitrine pour particuliers. Applications en Next.js et Typescript.
           </p>
-          <img src="https://les-devs-juniors.vercel.app/images/bachelor-cap.png" alt="LesdevsJuniorsLogo" />
+          <img className="lock" src="https://les-devs-juniors.vercel.app/images/bachelor-cap.png" alt="LesdevsJuniorsLogo" />
         </article>
       </a>
     </section>
